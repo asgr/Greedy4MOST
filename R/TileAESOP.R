1b050db0fc02a0b63b07adff522feddbe24dfbd1
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-08-20 13:51:43 +0800

    Yay, faster tiling! Smarter coordmatch kstart scaling. It used to be hard coded to WAVES-Wide, now it is more generic.

--- R/TileAESOP.R
+++ R/TileAESOP.R
@@ -31,13 +31,16 @@ TileAESOP=function(RA_data, Dec_data, weight_data=1, RAlo = 129, RAhi = 141, Dec
     tempgrid=rbind(expand.grid(seq(RAlo,360-grid,by=grid),seq(Declo,Dechi,by=grid)), expand.grid(seq(0,RAhi,by=grid),seq(Declo,Dechi,by=grid)))
   }
 
+  AESOP_area=pi*rad^2
+  survey_area=skyarea(c(RAlo,RAhi),c(Declo,Dechi))[1]
+
   tempdata=cbind(RA_data, Dec_data)
   select=selectRADec(RA_data,Dec_data,RAlo=RAlo,RAhi=RAhi,Declo=Declo,Dechi=Dechi)$select
   Nsamp=min(Nsamp, length(RA_data))
   select=select[sample(length(select),Nsamp)]
   tempdata=tempdata[select,]
   weight_data=weight_data[select]
-  match_mat=coordmatch(tempgrid, tempdata, rad=rad, radunit='deg', kstart = ceiling(Nsamp/20), smallapprox=TRUE)$ID
+  match_mat=coordmatch(tempgrid, tempdata, rad=rad, radunit='deg', kstart = ceiling(2*Nsamp*AESOP_area/survey_area), smallapprox=TRUE)$ID
   match_mat[match_mat>0]=weight_data[match_mat]
   Wmatch=rowSums(match_mat, na.rm=TRUE)
   Wmatch[!is.finite(Wmatch)]=0
